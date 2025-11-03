2fc443dd50b38b8a46d6e409f9740b5c13ed36e0
ArturPessoaCardoso <artur@germina.com.vc>
2025-11-03 16:39:55 -0300

    prisma client setup

--- test-app/app/page.tsx
+++ test-app/app/page.tsx
@@ -1,4 +1,17 @@
 import Image from "next/image";
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+const prisma = new PrismaClient().$extends(withAccelerate())
+
+const user = await prisma.user.findMany({
+  where: {
+    email: {
+      contains: 'alice@prisma.io',
+    },
+  },
+  cacheStrategy: { swr: 60, ttl: 60 },
+})
 
 export default function Home() {
   return (
@@ -14,7 +27,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            prisma:
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
